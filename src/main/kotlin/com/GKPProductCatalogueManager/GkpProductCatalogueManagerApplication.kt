07f2630c0e90ae6f084ad35e51b6bbf93d6f49eb
Gareth Park <garethpark@yahoo.com>
2024-03-19 13:36:35 +0000

    added initial files

---
+++ src/main/kotlin/com/GKPProductCatalogueManager/GkpProductCatalogueManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.GKPProductCatalogueManager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GkpProductCatalogueManagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GkpProductCatalogueManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/GKPProductCatalogueManager/GkpProductCatalogueManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.GKPProductCatalogueManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GkpProductCatalogueManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
