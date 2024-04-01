75c61786d15ef9f9ccf4c47b9a8201c14f62edf1
Gareth Park <garethpark@yahoo.com>
2024-04-01 15:01:02 +0100

    added getProductWithInvalidProductId_shouldReturnError

--- src/test/kotlin/com/GKPProductCatalogueManager/domain/services/unit/ProductServiceTest.kt
+++ src/test/kotlin/com/GKPProductCatalogueManager/domain/services/unit/ProductServiceTest.kt
@@ -7,6 +7,7 @@ import com.gkpProductCatalogueManager.testUtils.ProductDataFactory
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.*
 import java.util.Optional
 
@@ -29,5 +30,12 @@ class ProductServiceTest {
         `when`(productRepository.findById(anyLong())).thenReturn(Optional.of(existingProduct))
         val productResponse = productService.getProduct(1)
         assertThat(productResponse).isNotNull.extracting("id").isEqualTo(1L)
+    }
+
+    @Test
+    fun getProductWithInvalidProductId_shouldReturnError(){
+        assertThrows<NoSuchElementException> {
+            productService.getProduct(1)
+        }
     }
 }
