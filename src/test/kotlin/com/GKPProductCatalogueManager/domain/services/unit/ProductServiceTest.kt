4f6c26b4526d0142bae2968df7f53d81df366784
Gareth Park <garethpark@yahoo.com>
2024-04-01 14:43:38 +0100

    added ProductDataFactory

--- src/test/kotlin/com/GKPProductCatalogueManager/domain/services/unit/ProductServiceTest.kt
+++ src/test/kotlin/com/GKPProductCatalogueManager/domain/services/unit/ProductServiceTest.kt
@@ -1,24 +1,32 @@
 package com.gkpProductCatalogueManager.domain.services.unit
 
+import com.gkpProductCatalogueManager.domain.models.Product
+import com.gkpProductCatalogueManager.domain.repositories.ProductRepository
 import com.gkpProductCatalogueManager.domain.services.ProductService
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.mockito.Mockito.*
+import java.util.Optional
 
 
 class ProductServiceTest {
 
     private lateinit var productService: ProductService
+    private lateinit var productRepository: ProductRepository
 
     @BeforeEach
     fun setup(){
-        productService = ProductService()
+        productRepository = mock()
+        productService = ProductService(productRepository)
     }
 
     @Test
     fun getProductsWithValidProductId_shouldReturnProduct(){
+        val existingProduct
 
+        `when`(productRepository.findById(anyLong())).thenReturn(Optional.of(existingProduct))
         val productResponse = productService.getProduct(1)
-        assertThat(productResponse).isNotNull()  //.extracting("id").isEqualTo(1L)
+        assertThat(productResponse).isNotNull().extracting("id").isEqualTo(1L)
     }
 }
