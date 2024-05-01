96d45e68e04218e3b91834e428d1556c23c666f8
Gareth Park <garethpark@yahoo.com>
2024-05-01 21:39:54 +0100

    added identifier on product

--- src/main/kotlin/com/GKPProductCatalogueManager/domain/models/Product.kt
+++ src/main/kotlin/com/GKPProductCatalogueManager/domain/models/Product.kt
@@ -4,6 +4,8 @@ import jakarta.persistence.*
 @Entity
 @Table(schema = "PRODUCT_OWNER", name = "PRODUCT")
 data class Product(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     var id: Long? = null,
     @Column(name = "NAME")
