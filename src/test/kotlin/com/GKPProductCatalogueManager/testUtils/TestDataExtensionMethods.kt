cf0a4efa833f560f640e3b448d2eed844cc6f68f
Gareth Park <garethpark@yahoo.com>
2024-11-19 18:39:31 +0000

    make price nullable

--- src/test/kotlin/com/GKPProductCatalogueManager/testUtils/TestDataExtensionMethods.kt
+++ src/test/kotlin/com/GKPProductCatalogueManager/testUtils/TestDataExtensionMethods.kt
@@ -6,8 +6,8 @@ import java.math.BigDecimal
 class ProductDataFactory {
     class TestProduct{
         companion object{
-            fun validProduct(id: Long? = null, name: String? = null, price: BigDecimal): Product {
-                val product = Product(id = id, name = name, price = price)
+            fun validProduct(id: Long? = null, name: String? = null, price: BigDecimal? = null): Product {
+                val product = Product(id = id, name = name, price = price?: BigDecimal.ZERO)
                 return product
             }
         }
