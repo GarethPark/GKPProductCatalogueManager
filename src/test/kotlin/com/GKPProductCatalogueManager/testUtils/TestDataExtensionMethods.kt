fb779a4d4674e29f8cb89fc96922a6c3b8dc57b6
Gareth Park <garethpark@yahoo.com>
2024-11-19 18:34:04 +0000

    add price as parameter

--- src/test/kotlin/com/GKPProductCatalogueManager/testUtils/TestDataExtensionMethods.kt
+++ src/test/kotlin/com/GKPProductCatalogueManager/testUtils/TestDataExtensionMethods.kt
@@ -1,12 +1,13 @@
 package com.gkpProductCatalogueManager.testUtils
 
 import com.gkpProductCatalogueManager.domain.models.Product
+import java.math.BigDecimal
 
 class ProductDataFactory {
     class TestProduct{
         companion object{
-            fun validProduct(id: Long? = null, name: String? = null): Product {
-                val product = Product(id = id, name = name)
+            fun validProduct(id: Long? = null, name: String? = null, price: BigDecimal): Product {
+                val product = Product(id = id, name = name, price = price)
                 return product
             }
         }
